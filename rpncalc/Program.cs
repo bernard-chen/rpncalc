357da01536c70b53ab0e1a86af9e53613f582051
Bernard Chen <bernard@buildium.com>
2015-06-10 16:15:08 -0700

    Initial commit with addition supported

---
+++ rpncalc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace rpncalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<double>();
+
+            while (true)
+            {
+                Console.Write("> ");
+                var input = Console.ReadLine();
+
+                double number;
+                if (double.TryParse(input, out number))
+                {
+                    stack.Push(number);
+                }
+                else if (input == "+")
+                {
+                    var value1 = stack.Pop();
+                    var value2 = stack.Pop();
+                    stack.Push(value1 + value2);
+                }
+                Console.WriteLine(stack.AsString());
+            }
+        }
+    }
+}
---
+++ rpncalc/StackExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpncalc
+{
+    public static class StackExtensions
+    {
+        public static string AsString<T>(this Stack<T> stack)
+        {
+            return "[" + stack.Aggregate("]", (seed, next) => ", " + next + seed).TrimStart(',', ' ');
+        }
+    }
+}
